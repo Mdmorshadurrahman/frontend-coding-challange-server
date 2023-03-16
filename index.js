771d96e6ad164d9a1f5528673d4576c4281575e3
Mdmorshadurrahman <mdmorshadur.rahman@udo.edu>
2023-03-16 23:20:26 +0100

    displayed sphere geometry and also did radius adjustment function(task 5 & 6)

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-// const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
+// const http = require('http');
+// const Websocket = require('ws')
+// const wss = new Websocket.Server({PORT})
 
 app.use(cors())
 app.use(express.json())
+
+
+var currentValue = 0;
+
+
+app.post('/home', (req,res) =>{
+    const methodType = req.body.method;
+    console.log(methodType)
+    if(methodType == 'increment'){
+        currentValue += 1;
+    }
+    else{
+        currentValue -= 1;
+    }
+    console.log(currentValue);
+    res.send({success: true,
+        payload:currentValue})
+    });
+
+app.get('/home', (req,res) =>{
+    res.send({success: true,
+        payload:currentValue})
+        });
+
+
+
+app.get('/',(req,res) =>{
+    res.send('***Counter App Server is running***')
+    });
+
+app.listen(PORT, () => {
+    console.log(`Counter App Server is running.... at port ${PORT}`);
+
+});
+// const wsServer = new WebSocketServer({
+//     httpServer: wss
+// });
+// const clients = {};
+
+
+
+// wss.on('connection',(ws) => {
+//     console.log('counter app is connected')
+// })
+
+
+
+
+
 
 // const server = http.createServer(function (req, res){
 //     if(req.url ==='/home'){
@@ -31,39 +82,3 @@ app.use(express.json())
 //     console.log(error);
 // });
 
-var currentValue = 0;
-
-
-app.post('/home', (req,res) =>{
-    const methodType = req.body.method;
-    console.log(methodType)
-    // const recipe = await recipeCollection.insertOne(postRecipe);
-    if(methodType == 'increment'){
-        currentValue += 1;
-    }
-    else{
-        currentValue -= 1;
-    }
-    console.log(currentValue);
-    res.send({success: true,
-        payload:currentValue})
-        console.log(currentValue);
-    
-    });
-
-app.get('/home', (req,res) =>{
-    res.send({success: true,
-        payload:currentValue})
-        console.log(currentValue);
-        });
-
-
-
-app.get('/',(req,res) =>{
-    res.send('***Counter App Server is running***')
-    });
-
-app.listen(PORT, () => {
-    console.log(`Counter App Server is running.... at port ${PORT}`);
-
-});
